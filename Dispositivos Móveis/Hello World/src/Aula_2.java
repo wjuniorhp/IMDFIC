f8c15cdba642e5a5ea26f79982272d4a1438fbaf
Wellington Jr <wjuniorhp@gmail.com>
2021-04-19 22:35:22 -0300

    Initial coommit

---
+++ Dispositivos Móveis/Hello World/src/Aula_2.java
@@ -0,0 +1,24 @@
+public class Aula_2 {
+    public static int varGlobal = 5;
+    private static int varLocalClasse = 6;
+
+    public static void main(String[] args) {
+        int varLocalRotina = 10;
+
+        if (varGlobal == 0) {
+            int varLocalComando = varLocalRotina;
+            System.out.println(varLocalComando);
+        }
+        System.out.println(varLocalRotina);
+        // System.out.println(varLocalComando);
+        System.out.println(varGlobal);
+        System.out.println(varLocalClasse);
+    }
+
+    public static void rotina2() {
+        // System.out.println(varLocalRotina);
+        // System.out.println(varLocalComando);
+        System.out.println(varGlobal);
+        System.out.println(varLocalClasse);
+    }
+}
